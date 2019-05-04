51b988f84ec307c28a79457e903282612d776933
Kevin Blicharski <blicharskikevin@gmail.com>
2019-05-04 17:48:49 -0500

    feat: add csv library

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,3 @@
 open Lib
 
-let () = Fileio.read_file "../data/house-votes-84.data"
+let () = Fileio.read_file "data/house-votes-84.data"
--- lib/fileio.ml
+++ lib/fileio.ml
@@ -1,9 +1,11 @@
 let read_file (file_name: string) =
-  let in_channel = open_in file_name in
+  let data = Csv.load file_name in
+  Csv.print data
+  (* let in_channel = open_in file_name in
   try
     while true do
       let line = input_line in_channel in
       print_endline line
     done
   with End_of_file ->
-    close_in in_channel
+    close_in in_channel *)
