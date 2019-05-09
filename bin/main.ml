7f4b237e5b78727bea0327392ebe8d8f97b1d289
wheetree <derek-choi@uiowa.edu>
2019-05-09 00:51:13 -0500

    Added command line argument support

--- bin/main.ml
+++ bin/main.ml
@@ -5,8 +5,10 @@ open Model
 open DataModels
 open DecisionTree
 
-let () =
-  let data_file = "data/house-votes-84.data" in
+let createClassifier file =
+  (* let data_file = "data/votes-small.data" in *)
+  (* let data_file = "data/house-votes-84.data" in *)
+  let data_file =  (String.concat "" ["data/"; file]) in
   let ex = load_data data_file in
   let m = {
     positive = positive_for data_file;
@@ -22,3 +24,15 @@ let () =
   (* Verify that the sexp we wrote to a file is the same as the original *)
   assert (Sexplib.Sexp.compare sexp sexp2 = 0) ;
   ()
+
+
+let () =
+  match ((Array.length Sys.argv) - 1) with
+  | 0 -> 
+    Printf.printf "This program can be used with the following arguments: 
+    tree [dataFile]\n" (* add more usages later *)
+  | 2 ->
+    if (Array.get Sys.argv 1) = "tree" then
+      createClassifier (Array.get Sys.argv 2)
+      else failwith "not yet implemented"
+  | _ -> failwith "The given args do not fit any possible usages. To see program usages, execute without any arguments."
