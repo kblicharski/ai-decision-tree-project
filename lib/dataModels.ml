e54b31a927c95ba82bc40fb7e6e64541ac7f3973
Ted Paulsen <ted.m.paulsen@gmail.com>
2019-05-09 20:06:53 -0500

    add connect-4 model

--- lib/dataModels.ml
+++ lib/dataModels.ml
@@ -53,10 +53,58 @@ let characteristics4 = [
   ("Right-Distance", decisions4);
 ]
 
+let decisions5 = ["x";"o";"b"]
+let characteristics5 = [
+  ("class", ["win";"loss";"draw"]);
+  ("a1", decisions5);
+  ("a2", decisions5);
+  ("a3", decisions5);
+  ("a4", decisions5);
+  ("a5", decisions5);
+  ("a6", decisions5);
+  ("b1", decisions5);
+  ("b2", decisions5);
+  ("b3", decisions5);
+  ("b4", decisions5);
+  ("b5", decisions5);
+  ("b6", decisions5);
+  ("c1", decisions5);
+  ("c2", decisions5);
+  ("c3", decisions5);
+  ("c4", decisions5);
+  ("c5", decisions5);
+  ("c6", decisions5);
+  ("d1", decisions5);
+  ("d2", decisions5);
+  ("d3", decisions5);
+  ("d4", decisions5);
+  ("d5", decisions5);
+  ("d6", decisions5);
+  ("e1", decisions5);
+  ("e2", decisions5);
+  ("e3", decisions5);
+  ("e4", decisions5);
+  ("e5", decisions5);
+  ("e6", decisions5);
+  ("f1", decisions5);
+  ("f2", decisions5);
+  ("f3", decisions5);
+  ("f4", decisions5);
+  ("f5", decisions5);
+  ("f6", decisions5);
+  ("g1", decisions5);
+  ("g2", decisions5);
+  ("g3", decisions5);
+  ("g4", decisions5);
+  ("g5", decisions5);
+  ("g6", decisions5);
+]
+
 let characteristics_for file =
   match file with
   | "house-votes-84" | "votes-small" -> characteristics1
   | "car" -> characteristics2
   | "tic-tac-toe" -> characteristics3
   | "balance-scale" -> characteristics4
+  | "connect-4" -> characteristics5
   | _ -> failwith "data model not found"
